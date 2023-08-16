323665a35cb79bd495a9cd54013a9a90dffa868d
pedrolucas201 <quizueiramusj@gmail.com>
2023-08-16 15:22:17 -0300

    Comparador de Arrays em JAVA

---
+++ src/ArraysComparator1.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.util.Arrays;
+
+public class ArraysComparator1 {
+
+    public static void main(String[] args) {
+        int[] vet1;
+        vet1 = new int[5];
+        int[] vet2;
+        vet2 = new int[5];
+        int i;
+
+        for (i = 0; i < 1; i++){
+            String s_vet = JOptionPane.showInputDialog(null, "Digite 5 números: ");
+            vet1[5] = Integer.parseInt(s_vet);
+        }
+
+        for (i = 0; i < 1; i++){
+            String s_vet2 = JOptionPane.showInputDialog(null, "Digite 5 números novamente: ");
+            vet2[5] = Integer.parseInt(s_vet2);
+        }
+
+        if (Arrays.equals(vet1, vet2)){
+            JOptionPane.showMessageDialog(null,"As duas listas são iguais.");
+        }
+
+    }
+
+}
